85f4d502e2f5848703d1b16ae8723a3119c41b2b
EpicPuppy613 <61771471+EpicPuppy613@users.noreply.github.com>
2022-04-03 16:25:08 -0700

    V0.2.0 - added the rest of the animals

--- patterns.js
+++ patterns.js
@@ -28,7 +28,7 @@ G.patterns = {
                 ]
             },
             "cow": {
-                "id": "fc",
+                "id": "fcw",
                 "name": "Cow",
                 "size": [3, 1],
                 "pattern": [
@@ -51,6 +51,16 @@ G.patterns = {
                     [" ","X"],
                     ["X"," "],
                     ["X"," "]
+                ]
+            },
+            "chicken": {
+                "id": "fch",
+                "name": "Chicken",
+                "size": [3, 3],
+                "pattern": [
+                    ["X"," "," "],
+                    [" ","X"," "],
+                    [" "," ","X"]
                 ]
             }
         }
@@ -113,6 +123,16 @@ G.patterns = {
                     [" ", "X"],
                     ["X", " "]
                 ]
+            },
+            "echidna": {
+                "id": "oe",
+                "name": "Echidna",
+                "size": [3, 2],
+                "pattern": [
+                    ["X", " "],
+                    ["X", " "],
+                    [" ", "X"]
+                ]
             }
         }
     },
@@ -171,6 +191,15 @@ G.patterns = {
                     [" ", "X"],
                     ["X", " "],
                     [" ", "X"]
+                ]
+            },
+            "rhinoceros": {
+                "id": "sr",
+                "name": "Rhinoceros",
+                "size": [2, 3],
+                "pattern": [
+                    [" ", "X", " "],
+                    ["X", " ", "X"]
                 ]
             }
         }
@@ -232,6 +261,15 @@ G.patterns = {
                 "size": [1, 2],
                 "pattern": [
                     ["X", "X"]
+                ]
+            },
+            "otter": {
+                "id": "no",
+                "name": "Otter",
+                "size": [2, 2],
+                "pattern": [
+                    ["X", "X"],
+                    ["X", " "]
                 ]
             }
         }
@@ -296,6 +334,15 @@ G.patterns = {
                 "pattern": [
                     ["X", " ", "X"]
                 ]
+            },
+            "sowl": {
+                "id": "pso",
+                "name": "Snowy Owl",
+                "size": [2, 2],
+                "pattern": [
+                    [" ", "X"],
+                    ["X", "X"]
+                ]
             }
         }
     },
@@ -357,6 +404,15 @@ G.patterns = {
                     [" ", " ", "X"],
                     [" ", " ", " "],
                     ["X", " ", " "]
+                ]
+            },
+            "lemur": {
+                "id": "jl",
+                "name": "Lemur",
+                "size": [2, 3],
+                "pattern": [
+                    ["X", " ", "X"],
+                    [" ", "X", " "]
                 ]
             }
         }
@@ -422,12 +478,386 @@ G.patterns = {
                     [" ", " "],
                     ["X", " "]
                 ]
+            },
+            "ankylosaurus": {
+                "id": "da",
+                "name": "Ankylosaurus",
+                "size": [3, 2],
+                "pattern": [
+                    ["X", " "],
+                    [" ", " "],
+                    ["X", "X"]
+                ]
             }
         }
     },
-    "ice": {},
-    "city": {},
-    "mountain": {},
-    "moon": {},
-    "mars": {}
+    "ice": {
+        "animals": {
+            "wrhino": {
+                "id": "iwr",
+                "name": "Wooly Rhino",
+                "size": [4, 3],
+                "pattern": [
+                    [" ", "X", " "],
+                    ["X", " ", " "],
+                    [" ", " ", "X"],
+                    [" ", "X", " "]
+                ]
+            },
+            "gsloth": {
+                "id": "igs",
+                "name": "Giant Sloth",
+                "size": [3, 3],
+                "pattern": [
+                    ["X", " ", "X"],
+                    [" ", " ", " "],
+                    ["X", "X", " "]
+                ]
+            },
+            "dwolf": {
+                "id": "idw",
+                "name": "Dire Wolf",
+                "size": [4, 3],
+                "pattern": [
+                    [" ", "X", " "],
+                    ["X", " ", "X"],
+                    [" ", " ", " "],
+                    [" ", "X", " "]
+                ]
+            },
+            "stooth": {
+                "id": "ist",
+                "name": "Saber Tooth",
+                "size": [3, 3],
+                "pattern": [
+                    ["X", " ", " "],
+                    [" ", " ", "X"],
+                    [" ", "X", " "]
+                ]
+            },
+            "mammoth": {
+                "id": "im",
+                "name": "Mammoth",
+                "size": [3, 3],
+                "pattern": [
+                    [" ", "X", " "],
+                    [" ", " ", "X"],
+                    ["X", " ", " "]
+                ]
+            },
+            "akhlut": {
+                "id": "ia",
+                "name": "Akhlut",
+                "size": [3, 3],
+                "pattern": [
+                    [" ", "X", " "],
+                    [" ", " ", " "],
+                    ["X", " ", "X"]
+                ]
+            },
+            "ycamel": {
+                "id": "iyc",
+                "name": "Yukon Camel",
+                "size": [4, 3],
+                "pattern": [
+                    [" ", "X", " "],
+                    [" ", " ", " "],
+                    [" ", " ", "X"],
+                    ["X", " ", " "]
+                ]
+            }
+        }
+    },
+    "city": {
+        "animals": {
+            "raccoon": {
+                "id": "crc",
+                "name": "Raccoon",
+                "size": [4, 2],
+                "pattern": [
+                    ["X", "X"],
+                    [" ", " "],
+                    [" ", "X"],
+                    ["X", " "]
+                ]
+            },
+            "pigeon": {
+                "id": "cp",
+                "name": "Pigeon",
+                "size": [3, 3],
+                "pattern": [
+                    [" ", " ", "X"],
+                    [" ", "X", " "],
+                    ["X", "X", " "]
+                ]
+            },
+            "rat": {
+                "id": "crt",
+                "name": "Rat",
+                "size": [4, 2],
+                "pattern": [
+                    [" ", "X"],
+                    ["X", "X"],
+                    [" ", " "],
+                    ["X", " "]
+                ]
+            },
+            "squirrel": {
+                "id": "cs",
+                "name": "Squirrel",
+                "size": [3, 3],
+                "pattern": [
+                    [" ", "X", " "],
+                    ["X", " ", " "],
+                    [" ", " ", "X"]
+                ]
+            },
+            "opossum": {
+                "id": "co",
+                "name": "Opossum",
+                "size": [3, 2],
+                "pattern": [
+                    ["X", "X"],
+                    [" ", " "],
+                    ["X", " "]
+                ]
+            },
+            "sturtle": {
+                "id": "ct",
+                "name": "Sewer Turtle",
+                "size": [2, 1],
+                "pattern": [
+                    ["X"],
+                    ["X"]
+                ]
+            },
+            "chipmunk": {
+                "id": "cc",
+                "name": "Chipmunk",
+                "size": [4, 2],
+                "pattern": [
+                    ["X", " "],
+                    [" ", "X"],
+                    [" ", " "],
+                    ["X", " "]
+                ]
+            }
+        }
+    },
+    "mountain": {
+        "animals": {
+            "goat": {
+                "id": "mg",
+                "name": "Goat",
+                "size": [3, 2],
+                "pattern": [
+                    ["X", "X"],
+                    ["X", " "],
+                    ["X", " "]
+                ]
+            },
+            "cougar": {
+                "id": "mcg",
+                "name": "Cougar",
+                "size": [3, 3],
+                "pattern": [
+                    ["X", " ", "X"],
+                    [" ", "X", " "],
+                    ["X", " ", " "]
+                ]
+            },
+            "elk": {
+                "id": "mek",
+                "name": "Elk",
+                "size": [3, 2],
+                "pattern": [
+                    [" ", "X"],
+                    ["X", " "],
+                    ["X", "X"]
+                ]
+            },
+            "eagle": {
+                "id": "meg",
+                "name": "Eagle",
+                "size": [2, 3],
+                "pattern": [
+                    [" ", "X", "X"],
+                    ["X", " ", " "]
+                ]
+            },
+            "coyote": {
+                "id": "mcy",
+                "name": "Coyote",
+                "size": [3, 2],
+                "pattern": [
+                    [" ", "X"],
+                    [" ", "X"],
+                    ["X", " "]
+                ]
+            },
+            "aatxe": {
+                "id": "ma",
+                "name": "Aatxe",
+                "size": [3, 2],
+                "pattern": [
+                    ["X", "X"],
+                    [" ", " "],
+                    [" ", "X"]
+                ]
+            },
+            "pika": {
+                "id": "mp",
+                "name": "Pika",
+                "size": [3, 2],
+                "pattern": [
+                    [" ", "X"],
+                    [" ", " "],
+                    ["X", "X"]
+                ]
+            }
+        }
+    },
+    "moon": {
+        "animals": {
+            "moonkey": {
+                "id": "slmk",
+                "name": "Moonkey",
+                "size": [3, 3],
+                "pattern": [
+                    [" ", "X", "X"],
+                    [" ", " ", " "],
+                    ["X", "X", " "]
+                ]
+            },
+            "ltick": {
+                "id": "sll",
+                "name": "Lunar Tick",
+                "size": [3, 4],
+                "pattern": [
+                    ["X", " ", " ", " "],
+                    [" ", "X", " ", "X"],
+                    ["X", " ", " ", " "]
+                ]
+            },
+            "tribble": {
+                "id": "slt",
+                "name": "Tribble",
+                "size": [3, 2],
+                "pattern": [
+                    ["X", " "],
+                    ["X", "X"],
+                    ["X", " "]
+                ]
+            },
+            "moonicorn": {
+                "id": "slc",
+                "name": "Moonicorn",
+                "size": [2, 2],
+                "pattern": [
+                    ["X", "X"],
+                    ["X", " "]
+                ]
+            },
+            "lmoth": {
+                "id": "slmh",
+                "name": "Lunar Moth",
+                "size": [3, 3],
+                "pattern": [
+                    [" ", " ", "X"],
+                    ["X", " ", " "],
+                    [" ", " ", "X"]
+                ]
+            },
+            "jrabbit": {
+                "id": "slr",
+                "name": "Jade Rabbit",
+                "size": [2, 3],
+                "pattern": [
+                    [" ", " ", "X"],
+                    ["X", " ", " "]
+                ]
+            },
+            "babmoon": {
+                "id": "slb",
+                "name": "Babmoon",
+                "size": [3, 3],
+                "pattern": [
+                    ["X", " ", " "],
+                    [" ", " ", "X"],
+                    [" ", "X", " "]
+                ]
+            }
+        }
+    },
+    "mars": {
+        "animals": {
+            "marsmot": {
+                "id": "smmt",
+                "name": "Marsmot",
+                "size": [2, 3],
+                "pattern": [
+                    ["X", " ", " "],
+                    ["X", "X", "X"]
+                ]
+            },
+            "marsmoset": {
+                "id": "smms",
+                "name": "Marsmoset",
+                "size": [3, 3],
+                "pattern": [
+                    [" ", " ", "X"],
+                    ["X", " ", " "],
+                    [" ", "X", "X"]
+                ]
+            },
+            "rock": {
+                "id": "smrk",
+                "name": "Rock",
+                "size": [2, 2],
+                "pattern": [
+                    ["X", "X"],
+                    ["X", "X"]
+                ]
+            },
+            "rover": {
+                "id": "smrv",
+                "name": "Rover",
+                "size": [3, 2],
+                "pattern": [
+                    ["X", " "],
+                    [" ", "X"],
+                    ["X", " "]
+                ]
+            },
+            "martian": {
+                "id": "smmn",
+                "name": "Martian",
+                "size": [3, 2],
+                "pattern": [
+                    [" ", "X"],
+                    ["X", " "],
+                    [" ", "X"]
+                ]
+            },
+            "marsmallow": {
+                "id": "smml",
+                "name": "Marsmallow",
+                "size": [1, 3],
+                "pattern": [
+                    ["X", " ", "X"]
+                ]
+            },
+            "marsten": {
+                "id": "smmr",
+                "name": "Marsten",
+                "size": [4, 2],
+                "pattern": [
+                    [" ", "X"],
+                    [" ", " "],
+                    [" ", "X"],
+                    ["X", " "]
+                ]
+            }
+        }
+    }
 }

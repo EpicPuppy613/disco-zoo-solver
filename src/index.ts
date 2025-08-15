f4c14594b644e5ffeb033a1917b9f2d67df40bad
EpicPuppy613 <epicpuppy@epicpuppy.dev>
2025-08-15 07:24:01 -0700

    Begin UI Rework

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ class GridCell {
     current: CellAnimal = CellAnimal.Unknown;
     totalChances: number = 0;
     animalChances: number[] = [];
+    element: HTMLTableCellElement;
 }
 
 class Animal {
@@ -33,4 +34,20 @@ function reset() {
         grid[i] = new Array(5).fill(0).map(e => new GridCell());
     }
     animals = [];
+}
+
+// First time init
+reset();
+const gridTable = document.getElementById("grid-table") as HTMLTableElement;
+const gridRows: HTMLTableRowElement[] = [];
+for (let i = 0; i < 5; i++) {
+    gridRows.push(document.createElement("tr"));
+    gridTable.appendChild(gridRows[i]);
+}
+for (let x = 0; x < 5; x++) {
+    for (let y = 0; y < 5; y++) {
+        grid[x][y].element = document.createElement("td");
+        gridRows[y].appendChild(grid[x][y].element);
+        grid[x][y].element.innerHTML = `<strong class="best">0.0%</strong><br><span>Pig: 0.0%</span><br><span>Sheep: 0.0%</span>`;
+    }
 }
